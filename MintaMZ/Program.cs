0dc00588aa6c5ffc19e84465464aeb8bf0227b1a
TamasNyirati <tamas.nyirati@gmail.com>
2021-04-12 18:07:40 +0200

    szélső értékek

--- MintaMZ/Program.cs
+++ MintaMZ/Program.cs
@@ -9,15 +9,36 @@ namespace MintaMZ
 {
     class Program
     {
+        class Auto
+        {
+            public int ertek;
+        }
         static void Main(string[] args)
         {
             //fájlbeolvasás
             string [] sorok = File.ReadAllLines("forras.txt");
-
+            List<Auto> autok = new List<Auto>();
+            foreach (string sor in sorok.Skip(1)) //skip, ha van fejléc
+            { 
+                autok.Add(new Auto());
+            }
             //adatok száma
-            int N = sorok.Length; //Ha van fejléc, akkor -1
+            int N = autok.Count; //Ha van fejléc, akkor -1
             //legtöbb vmi
-
+            /*szélső érték keresés*/
+            int minIndex = 0,  maxIndex = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if(autok[i].ertek > autok[maxIndex].ertek)
+                {
+                    maxIndex = i;
+                }
+                if (autok[i].ertek < autok[minIndex].ertek)
+                {
+                    minIndex = i;
+                }
+            }
+            Console.WriteLine($"a legnagyobb érték: {maxIndex}, a legkisebb érték: {minIndex}");
             //van-e egy valamilyen
         }
     }
